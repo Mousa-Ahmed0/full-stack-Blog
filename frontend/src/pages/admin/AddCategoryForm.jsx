0ff90cdb287046859d4965e0f36bdbebcdcb8781
Mousa-Ahmed0 <mousa.progm@gmail.com>
2023-10-14 20:34:08 +0300

    Admin Dashboard Page

--- frontend/src/pages/admin/AddCategoryForm.jsx
+++ frontend/src/pages/admin/AddCategoryForm.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+
 const AddCategoryForm = () => {
+    const [title,setTitle]=useState("");
+
+    //Form submit handler
+    const formSubmitHandler=(e)=>{
+        e.preventDefault();
+        if(title.trim()==='') return toast.error("Category title is required");
+        console.log({title});
+    }
     return ( 
         <div className="add-category">
             <h6 className="add-category-title">Add New Category</h6>
-            <form className="add-category-form">
+            <form onSubmit={formSubmitHandler} >
                 <div className="add-category-form-group">
                     <label htmlFor="title">Category Title</label>
-                    <input type="text" id="title" placeholder="Enter Category Title" />
+                    <input type="text" id="title" placeholder="Enter Category Title" onChange={(e)=>setTitle(e.target.value)}/>
                 </div>
                 <button className="add-category-btn" type="submit">Add </button>
             </form>
