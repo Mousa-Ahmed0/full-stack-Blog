25e7eb7ad8c371540af955d0547fc79cb0fcbfc2
Mousa-Ahmed0 <mousa.progm@gmail.com>
2023-10-16 12:01:33 +0300

    Forms

--- frontend/src/pages/Forms/Login.jsx
+++ frontend/src/pages/Forms/Login.jsx
@@ -1,6 +1,42 @@
+import { Link } from "react-router-dom";
+import './form.css'
+import { useState } from "react";
+import { toast } from "react-toastify";
+ 
 const Login = () => {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
+    //form submit handler
+    const formSubmitHandler =(e)=>{
+        e.preventDefault();
+        if(email.trim()==='') return toast.error("Email is required");
+        if(password.trim()==='') return toast.error("Password is required");
+        console.log({email,password});
+    }
     return ( 
-        <h1>Login</h1>
+        <section className="form-container">
+            <h1 className="form-title">Login to your account</h1>
+            <form onSubmit={formSubmitHandler} className="form">
+          
+                <div className="form-group">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-input" id="email" placeholder="Enter your email" 
+                        value={email} onChange={(e)=>setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-input" id="password" placeholder="Enter your password" 
+                        value={password} onChange={(e)=>setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="form-btn">Login</button>
+            </form>
+            <div className="form-footer">
+                Did you forgot your password? <Link to='/forgot-password'>Forgot Password</Link>
+            </div>
+        </section>
      );
 }
  
