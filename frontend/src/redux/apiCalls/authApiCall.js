75819d4cf3885c431bba558adc36820f6fd2ac95
Mousa-Ahmed0 <mousa.progm@gmail.com>
2023-10-24 14:22:39 +0300

    Axios

--- frontend/src/redux/apiCalls/authApiCall.js
+++ frontend/src/redux/apiCalls/authApiCall.js
@@ -1,20 +1,24 @@
+import request from "../../utils/request";
 import { authActons } from "../slices/authSlice";
-
+import {toast} from "react-toastify";
 //Login user
 export function loginuser(user){
     return async(dispatch)=>{
         try{
-            const response=await fetch('http://localhost:5000/api/auth/login',{
-                method:"POST",
-                body:JSON.stringify(user),
-                headers:{'Content-Type':"application/json"}
-            })
-            const data =await response.json();
+            // const response=await fetch('http://localhost:5000/api/auth/login',{
+            //     method:"POST",
+            //     body:JSON.stringify(user),
+            //     headers:{'Content-Type':"application/json"}
+            // })
+            // const data =await response.json();
+
+            const {data}=await request.post('/api/auth/login',user)
+
             dispatch(authActons.login(data));
             localStorage.setItem('userInfo',JSON.stringify(data));
 
         }catch(error){
-            console.log(error);
+            toast.error(error.response.data.message);
         }
     }
 }
