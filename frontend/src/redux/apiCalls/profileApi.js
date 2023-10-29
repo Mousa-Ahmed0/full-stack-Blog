d89b8558df9f05dbd0cb8a584bc2b11123bae3ef
Mousa-Ahmed0 <mousa.progm@gmail.com>
2023-10-29 15:31:48 +0200

    Fixing Some Bugs

--- frontend/src/redux/apiCalls/profileApi.js
+++ frontend/src/redux/apiCalls/profileApi.js
@@ -1,7 +1,20 @@
 import request from "../../utils/request";
 import { profileActons } from "../slices/profileSlice";
 import { toast } from "react-toastify";
-//Get userprofile
+//Get user profile
+export function getUserProfile(userId) {
+    return async (dispatch) => {
+        try {
+            const { data } = await request.get(`/api/users/Profile/${userId}`);
+
+            dispatch(profileActons.setProfile(data));
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
+}
+
+//Get user profile
 export function getUserProfile(userId) {
     return async (dispatch) => {
         try {
